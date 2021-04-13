dcdab783a67f625f36101968f2ce6869101d65e3
Andres Pacheco <andres.pacheco@the-cocktail.com>
2021-04-13 12:32:20 +0200

    list subbrees and amount of breeds

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,8 +11,19 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('/breedImages/:breed')
-  getBreedImages(@Param('breed') idBreed: string) {
-    return this.appService.getBreedImages(idBreed);
+  
+//obetener lista de iomagenes segun raza
+@Get('/breedImages/:breed')
+getBreedImages(@Param('breed') idBreed: string) {
+  return this.appService.getBreedImages(idBreed);
+}
+
+
+//obtener lista de sub-raza de perro
+  @Get('/breed/:breed')
+  getListBreed(@Param('breed') breed: string) {
+    return this.appService.getsubbreedList(breed);
   }
+
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import Axios from 'axios';
+import { json } from 'express';
 
 @Injectable()
 export class AppService {
@@ -8,15 +9,48 @@ export class AppService {
     return 'My first Nesttt';
   }
 
+
   //metodo para acceder a los servicios externos, asi obtengo las imagenes
   async getBreedImages(breed: string) {
-    //Axios.get("https://dog.ceo/api/breed/hound/images");
 
     //usamos await porque esperamos a obtener la respuesta
     const result = await Axios.get(`https://dog.ceo/api/breed/${breed}/images`);
+    //asi accedo a las imagenes externas
 
-    //mostramos por consola los datos de las imagenes usando .data
+    
     console.log(result.data);
+  //mostramos por consola los datos de las imagenes usando .data
     return result.data;
   }
+  
+
+  async getsubbreedList(breed: string) {
+    const subresult=await Axios.get(`https://dog.ceo/api/breed/${breed}/list`);
+    
+    const list:string[] =subresult.data.message;
+    const amount= list.length;
+    //var list= JSON.parse(subresult.data);
+
+    //console.log(subresult.data);
+
+    /*
+    Axios.get(`https://dog.ceo/api/breed/${breed}/list`)
+    .then (res =>console.log(res));
+*/
+    console.log(subresult.data.message.length);
+    //var amount=  Array((await Axios.get(https://dog.ceo/api/breed/${breed}/list')).data.length);
+    console.log(list);
+
+    return  {
+      breeds:list,
+      amountt:amount
+      };
+   
+  }
+
+
+
+}
+function created() {
+  throw new Error('Function not implemented.');
 }
