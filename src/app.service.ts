9b6a244f7bbd5625aa4f44c3ee5d45178db0b754
Andres Pacheco <andres.pacheco@the-cocktail.com>
2021-04-15 12:52:50 +0200

    add new coments

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Axios from 'axios';
 import { json } from 'express';
+import { findSourceMap } from 'node:module';
 
 @Injectable()
 export class AppService {
@@ -8,36 +9,44 @@ export class AppService {
 
 //metodo para saber el numero de razas que queramos 
   async getBreedsNumber(breedsNumber: number){
-    //obtenemos array geeral con razas pero JSON
+    //obtenemos objeto JSON con la lista entera de razas
     const breedsJson= await (await Axios.get(`https://dog.ceo/api/breeds/list/all`)).data.message;
 
-    //parseamos JSON a Array
+    //parseamos JSON a Array obteniendo un array nuevo 
     const arrAllBreeds = Object.keys(breedsJson);
-    //declaramos array con la longitus del numero introducido
+    
+    //console.log(arrAllBreeds);
 
-    //let arrNumber=[].length=breedsNumber;
+    //declaramos array con la longitus del numero introducido      //let arrNumber=[].length=breedsNumber;
     let arrNumber=[];
     arrNumber.length=breedsNumber;
-    //console.log(arrnumber.length);
 
-
+    //decalramos el array final que tendremos 
     let finalArray=[];
-    //llenamos el array razas aleatorias con numeros random 
+
+
+
+    //llenamos el array razas aleatorias con numeros random
     for(var i=0;i<arrNumber.length;i++){
       var breed =Math.floor(Math.random()*arrAllBreeds.length+1);
       arrNumber[i]=arrAllBreeds[breed];
-      var breedImage=await (await Axios.get(`https://dog.ceo/api/breed/${arrAllBreeds[breed]}/images/random`)).data.message;
+      var breedImage: String =await (await Axios.get(`https://dog.ceo/api/breed/${arrAllBreeds[breed]}/images/random`)).data.message;
       const subBreed:string[] = await (await Axios.get(`https://dog.ceo/api/breed/${arrAllBreeds[breed]}/list`)).data.message;
+
+
       const a={
         finalBreed:arrNumber[i],
         finaSsubBreed:subBreed,
         finalUrlImage:breedImage
       }
+
+
       finalArray.push(a);
       //console.log(arrNumber,breedImage,subBreed)
     }
     return finalArray;
   }
+  
 }
 
 function created() {
